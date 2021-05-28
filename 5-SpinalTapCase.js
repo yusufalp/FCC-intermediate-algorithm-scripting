2bb5bcc87fcd3309f5ad5e432cdbdf9cc21b5329
Yusuf Alp <alpyusuf007@gmail.com>
2021-05-28 13:02:14 -0700

    Spinal Tap Case Solution

--- 5-SpinalTapCase.js
+++ 5-SpinalTapCase.js
@@ -1,34 +1,12 @@
-/**
- * Convert a string to spinal case. Spinal case is 
- * all-lowercase-words-joined-by-dashes.
- */
-
 function spinalCase(str) {
-  let result = "";
-  for (let i = 0; i < str.length; i++) {
-    if (i === 0 && /[A-Z]+/.test(str[i])) {
-      result += str[i].toLowerCase();
-    } else if (/[-_\s]+/.test(str[i - 1])) {
-      result += `-${str[i].toLowerCase()}`;
-    } else if (!(/[-_\s]+/.test(str[i - 1])) && /[A-Z]+/.test(str[i])) {
-      result += `-${str[i].toLowerCase()}`;
-    } else if (/[-_\s]+/.test(str[i])) {
-      // result += "-";
-    } else {
-      result += str[i];
-    }
-  }
-  console.log(result);
-
-
-
-  //  let checkSpecial = /[*-_\s]+/.test(str)
-  //  let checkUpper = /[A-Z]+/.test(str)
-  //  console.log(checkSpecial, checkUpper)
+  return str
+    .split(/\s|_|(?=[A-Z])/)
+    .join("-")
+    .toLowerCase();
 }
 
 spinalCase("This Is Spinal Tap")
-//should return the string this-is-spinal-tap.
+// should return the string this-is-spinal-tap.
 
 spinalCase("thisIsSpinalTap")
 // should return the string this-is-spinal-tap.
