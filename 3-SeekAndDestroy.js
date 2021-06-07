2793231ced5ff4b1589abd569aa7cdfc189707d3
Yusuf Alp <alpyusuf007@gmail.com>
2021-06-07 14:58:39 -0700

    Seek and Destroy Solution

--- 3-SeekAndDestroy.js
+++ 3-SeekAndDestroy.js
@@ -14,16 +14,29 @@ function destroyer(arr) {
   let result = [];
   for (let i = 0; i < arr.length; i++) {
     if (!restOfArgs.includes(arr[i])) {
-      result.push(arr[i])
+      result.push(arr[i]);
     }
   }
   return result;
 }
 
-destroyer([1, 2, 3, 1, 2, 3], 2, 3); // should return [1, 1].
-destroyer([1, 2, 3, 1, 2, 3], 2, 3) //should return [1, 1].
-destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3) //should return [1, 5, 1].
-destroyer([3, 5, 1, 2, 2], 2, 3, 5) //should return [1].
-destroyer([2, 3, 2, 3], 2, 3) //should return [].
-destroyer(["tree", "hamburger", 53], "tree", 53) //should return ["hamburger"].
-destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan") //should return [12,92,65].
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+// should return [1, 1]
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+// should return [1, 1]
+
+destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3);
+// should return [1, 5, 1]
+
+destroyer([3, 5, 1, 2, 2], 2, 3, 5);
+// should return [1]
+
+destroyer([2, 3, 2, 3], 2, 3);
+// should return []
+
+destroyer(["tree", "hamburger", 53], "tree", 53);
+// should return ["hamburger"]
+
+destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan")
+// should return [12, 92, 65].
